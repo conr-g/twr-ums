1c82455c986fda7f3fcd7ab16ce9814ac458c431
James Hemming <James.Hemming@euro.ngc.com>
2019-05-02 13:50:44 +0100

    Added SBT plugin to allow compilation within VSCode. Added some initial controller methods.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -5,33 +5,26 @@ import play.mvc.*;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoIterable;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ConnectionString;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 public class HomeController extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result index() {
-        return ok(views.html.index.render());
+        return ok();
     }
 
-    public Result db() {
+    public Result listDatabases() {
         MongoClient mongoClient = MongoClients.create();
         MongoIterable<String> databases = mongoClient.listDatabaseNames();
         for (String database : databases) {
             System.out.println(database);
         }
+        return ok();
+    }
+
+    public Result createUser() {
+        return ok();
+    }
+
+    public Result login() {
         return ok();
     }
 }
