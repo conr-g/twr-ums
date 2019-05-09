e21ee29fbdbe7a49cc487cc59c0749680395bbbb
MattPotticary <mattpotticary@gmail.com>
2019-05-09 10:58:44 +0100

    db returns json instead writing to console (#1)
    * \db returns json instead writing to console
    
    * Removed unused import
    
    * Moved mongo client creation to constructor

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,30 @@
 package controllers;
 
-import play.mvc.*;
+import java.util.List;
 
+import play.mvc.*;
+import play.libs.Json;
+
+import com.google.common.collect.Lists;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoIterable;
 
 public class HomeController extends Controller {
+    private MongoClient mongoClient;
+
+    public HomeController(){
+        mongoClient = MongoClients.create();
+    }
+
     public Result index() {
         return ok();
     }
 
     public Result listDatabases() {
-        MongoClient mongoClient = MongoClients.create();
         MongoIterable<String> databases = mongoClient.listDatabaseNames();
-        for (String database : databases) {
-            System.out.println(database);
-        }
-        return ok();
+        List<String> tmp = Lists.newArrayList(databases);
+        return ok(Json.toJson(tmp));
     }
 
     public Result createUser() {
