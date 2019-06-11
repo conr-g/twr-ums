c13e80d2c04de247d249a1c8d53be37b26a416c5
James Hemming <James.Hemming@euro.ngc.com>
2019-06-11 15:07:20 +0100

    Removed obsolete code from HomeController as it now resides in UserController.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,35 +3,21 @@ package controllers;
 import java.util.List;
 
 import play.mvc.*;
-import play.data.Form;
-import play.data.FormFactory;
 
 import com.google.inject.Inject;
 import play.libs.Json;
 
 import com.google.common.collect.Lists;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoIterable;
-import static com.mongodb.client.model.Filters.eq;
-
-import org.bson.Document;
-
-import model.User;
-
 
 public class HomeController extends Controller {
-    private FormFactory formFactory;
     private MongoClient mongoClient;
-    private MongoDatabase database;
 
     @Inject
-    public HomeController(FormFactory formFactory) {
-        this.formFactory = formFactory;
+    public HomeController() {
         mongoClient = MongoClients.create();
-        database = mongoClient.getDatabase("waitingroom");
     }
 
     public Result index() {
@@ -42,34 +28,5 @@ public class HomeController extends Controller {
         MongoIterable<String> databases = mongoClient.listDatabaseNames();
         List<String> tmp = Lists.newArrayList(databases);
         return ok(Json.toJson(tmp));
-    }
-
-    public Result createUser(Http.Request request) {
-        Form<User> userForm = formFactory.form(User.class).bindFromRequest(request);
-
-       /* Document document = new Document("username", userForm.get().getUsername())
-                                 .append("password", userForm.get().getPassword());
-
-        MongoCollection<Document> collection = database.getCollection("users");
-        collection.insertOne(document);*/
-
-        return ok();
-    }
-
-    public Result login(Http.Request request) {
-       /* Form<User> userForm = formFactory.form(User.class).bindFromRequest(request);
-
-        MongoCollection<Document> collection = database.getCollection("users");
-        Document user = collection.find(eq("username", userForm.get().getUsername())).first();
-
-        if (user != null && user.get("password").equals(userForm.get().getPassword())) {
-            System.out.println("Login successful");
-            return ok(Json.toJson(user));
-        } else {
-
-            System.out.println("Login failed");
-            */
-            return ok();
-
     }
 }
